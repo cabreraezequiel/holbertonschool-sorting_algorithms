bf817ea0782b398bf67dc917447a9af3f38cd88e
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-06-13 18:44:35 -0700

    Create 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending
+ * order using the Insertion sort algorithm
+ *
+ * @list: list
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	int n = 0;
+
+	while (i < size - 1)
+	{
+		n = i;
+		j = i + 1;
+		while (j < size)
+		{
+			if (array[j] < array[x])
+				n = j;
+			j++;
+		}
+	}
+}
