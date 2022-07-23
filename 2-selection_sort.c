d111f0cd486a673e145138294397ad5a617c0b17
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-22 23:07:44 -0700

    Create 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
  * insertion_sort_list - sorts a doubly linked list of integers in ascending
@@ -9,18 +10,28 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i = 0;
-	int n = 0;
+	size_t i = 0, j = 0;
+	int n = 0, pos = 0;
 
+	if (!array)
+		return;
 	while (i < size - 1)
 	{
-		n = i;
+		n = array[i];
 		j = i + 1;
 		while (j < size)
 		{
-			if (array[j] < array[x])
-				n = j;
+			if (n > array[j])
+			{
+				n = array[j];
+				pos = j; }
 			j++;
 		}
+		if (array[pos] != array[i])
+		{
+			array[pos] = array[i];
+			array[i] = n;
+			print_array(array, size); }
+		i++;
 	}
 }
